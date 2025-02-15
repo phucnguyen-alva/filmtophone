e823dfa2ccf6f2cd5f0d10076a1ffaf3be9ff7ed
Alva <143425737+phucnguyen-alva@users.noreply.github.com>
2025-02-15 17:07:20 +0000

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
-
-// The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
+  // Copy images folder
   eleventyConfig.addPassthroughCopy("images");
+
+  // Copy styles folder
   eleventyConfig.addPassthroughCopy("styles");
+
+  return {
+      dir: {
+          input: ".",
+          includes: "_includes",
+          output: "_site"
+      }
+  };
 };
